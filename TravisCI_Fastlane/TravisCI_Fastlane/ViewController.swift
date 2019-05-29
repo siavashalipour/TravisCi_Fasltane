ebb8110f9f6ad5bb6977a1d642b0f2bcb486ed18
Siavash <>
2019-05-29 10:11:12 +1000

    added the project file

---
+++ TravisCI_Fastlane/TravisCI_Fastlane/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TravisCI_Fastlane
+//
+//  Created by Siavash on 29/5/19.
+//  Copyright © 2019 Siavash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ TravisCI_Fastlane/TravisCI_FastlaneTests/TravisCI_FastlaneTests.swift
@@ -0,0 +1,34 @@
+//
+//  TravisCI_FastlaneTests.swift
+//  TravisCI_FastlaneTests
+//
+//  Created by Siavash on 29/5/19.
+//  Copyright © 2019 Siavash. All rights reserved.
+//
+
+import XCTest
+@testable import TravisCI_Fastlane
+
+class TravisCI_FastlaneTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
